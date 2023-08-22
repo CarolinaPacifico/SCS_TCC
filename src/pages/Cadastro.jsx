5879481436b224ea75dcc971bc4e44bff3ec5f77
CarolinaPacifico <84384263+CarolinaPacifico@users.noreply.github.com>
2023-08-22 20:50:09 -0300

    Add files via upload

---
+++ src/pages/Cadastro.jsx
@@ -0,0 +1,12 @@
+
+
+function Cadastro() {
+
+    return (
+      <>
+        
+      </>
+    )
+  }
+  
+  export default Cadastro
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+//import '../Home.css'
+import NavBar from '../components/NavBar'
+//import NavBar from '../components/NavBar'
+
+function Home() {
+
+    return (
+      <>
+        <NavBar/>
+      </>
+    )
+  }
+  
+  export default Home
