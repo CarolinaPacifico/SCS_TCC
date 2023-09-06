fa74071f25327a67e721e18145b082e39ea0528a
CarolinaPacifico <84384263+CarolinaPacifico@users.noreply.github.com>
2023-09-06 16:20:08 -0300

    Add files via upload

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,75 +10,75 @@ import '../App.css'
 function Footer() {
 
     return (
-        <nav>
-            <div className="linha">
-            <img src={linhaFooter}/>
-            </div>
+        <nav className='footer'>
             
-        <div className="links-footer">
-        <Link href="/">
-                  <img
-                  src={LOGO}
-                  width="200"
-                  height="200"
-                  className="d-inline-block align-top"
-                  />
-          </Link>
+            <div className="links-footer">
+                <Link href="/">
+                        <img
+                        src={LOGO}
+                        width="200"
+                        height="200"
+                        className="d-inline-block align-top"
+                        />
+                </Link>
+                <div className='navs-footer'>
 
-        <div className='sobre-footer'>
-        <h1>Sobre</h1>
-        <p>Ajuda</p>
-        <p>Contratação</p>
-        <p>Sobre Nós</p>
-        </div>
+                    <div className='sobre-footer'>
+                        <h1>Sobre</h1>
+                        <p>Ajuda</p>
+                        <p>Contratação</p>
+                        <p>Sobre Nós</p>
+                    </div>
+                    <div className='info-footer'>
+                        <h1>Info</h1>
+                        <p>Fale Conosco</p>
+                        <p>Política de Privacidade</p>
+                        <p>Termos e Condições</p>
+                    </div>
+                    <div className='siga-footer'>
+                        <h1>Siga-nos</h1>
+                        <div>
+                            <Link href="/">
+                                    <img
+                                    src={whatsapp}
+                                    width="45"
+                                    height="45"
+                                    className="d-inline-block align-top"
+                                    />
+                            </Link>
 
-        <div className='info-footer'>
-        <h1>Info</h1>
-        <p>Fale Conosco</p>
-        <p>Política de Privacidade</p>
-        <p>Termos e Condições</p>
-        </div>
+                            <Link href="/">
+                                    <img
+                                    src={instagram}
+                                    width="42"
+                                    height="42"
+                                    className="d-inline-block align-top"
+                                    />
+                            </Link>
 
-        <div className='siga-footer'>
-        <h1>Siga-nos</h1>
-        <Link href="/">
-                  <img
-                  src={whatsapp}
-                  width="30"
-                  height="30"
-                  className="d-inline-block align-top"
-                  />
-          </Link>
-
-          <Link href="/">
-                  <img
-                  src={instagram}
-                  width="26"
-                  height="26"
-                  className="d-inline-block align-top"
-                  />
-          </Link>
-
-          <Link href="/">
-                  <img
-                  src={facebook}
-                  width="25"
-                  height="25"
-                  className="d-inline-block align-top"
-                  />
-          </Link>
-          </div>
-
-        <div className='siga-footer'>
-            <h1>Páginas</h1>
-                <div className="loginbtn-footer">
-                    <Link href="/Login">Login</Link>
-                    <Link href="/Cadastro">Cadastre-se</Link> 
+                            <Link href="/">
+                                    <img
+                                    src={facebook}
+                                    width="40"
+                                    height="40"
+                                    className="d-inline-block align-top"
+                                    />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='paginas-footer'>
+                        <h1>Páginas</h1>
+                            <div className="loginbtn-footer">
+                                <a href="/Login">Login</a>
+                            </div>
+                            <div className="cadastrobtn-footer">
+                                <a href="/Login">Cadastre-se</a> 
+                            </div>
+                        <p>Contato</p>
+                    </div>
                 </div>
-            <p>Contato</p>
-        </div>
-    </div>
-    </nav>
+            </div>
+        </nav>
     )
   }
   
--- src/components/profissionais.jsx
+++ src/components/profissionais.jsx
@@ -15,20 +15,20 @@ function Profissionais() {
                 </div>
 
                 <div className="cards container">
-                    <article className='especializacao-bg'>
-                        <img src={estetoscopio}/>
+                    <article className='profissionais-bg'>
+                        <img src={estetoscopio} width={90}height={90}/>
                         <h1>Especialização</h1>
                         <p>Organização proficientemente engajada em oferecer serviços de assistência em ambientes residenciais.</p>
                     </article>
 
-                    <article className='capacitacao-bg'>
-                        <img src={chapeu}/>
+                    <article className='profissionais-bg'>
+                        <img src={chapeu} width={90}height={90}/>
                         <h1>Capacitação</h1>
                         <p>Com a presença de profissionais qualificados, é possível obter o suporte necessário para o seu ente querido.</p>
                     </article>
 
-                    <article className='dedicacao-bg'>
-                        <img src={coracao}/>
+                    <article className='profissionais-bg'>
+                        <img src={coracao} width={90}height={90}/>
                         <h1>Dedicação</h1>
                         <p>A afetuosidade e o empenho demonstrados durante o convívio são elementos distintivos em nossa gama de serviços.</p>
                     </article>
